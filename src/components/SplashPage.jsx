c523b72c0dea6ad3ee6cf52dea572f6ecdcc975c
Roger Brooks <rogerbrooks@cox.net>
2026-05-10 09:18:41 -0700

    splash text

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -5,7 +5,7 @@ export default function SplashPage({ onPlay }) {
     <div className="splash">
       <div className="splash-content">
         <p className="splash-message">
-          Some loves are so rare they leave a permanent mark on your soul. Ours was that.
+          Jackie, some loves are so rare they leave a permanent mark on your soul. Ours was that.
           Whatever the future holds I am grateful every day that I got to love you and be loved by you.
           Take care of yourself. You will always be in my heart.
         </p>
